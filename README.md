ebef759506d7f8286a647f88490f5e14f6387f43
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-11-03 13:35:02 +0300

    add project information to the readme file  new site information

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# hoodwatch
+# HOOD WATCH
+
+#### By **Jackson kitsao**
+
+## Description
+This is a web application that allows you to be in the loop about everything happening in your neighborhood. From contact information of different handyman to meeting announcements or even alerts.
+
+**A user can**
+* Sign in with the application to start using.
+* Set up a profile about me and a general location and my neighborhood name.
+* Find a list of different businesses in my neighborhood.
+* Find Contact Information for the health department and * Police authorities near my neighborhood.
+* Create Posts that will be visible to everyone in my neighborhood.
+* Change My neighborhood when I decide to move out.
+* Only view details of a single neighborhood.
+
+## Live Link
+https://hoodwatchs.herokuapp.com/
+
+## Set Up and Installations
+
+### Prerequisites
+1. Python3.6
+2. Postgres
+3. Python virtualenvironment
+### Clone the Repo
+
+Run the following command on the terminal:
+`https://github.com/jkitsao/HoodWatch.git`
+
+### Activate virtual environment
+Activate virtual environment using python3.6 as default handler
+```bash
+virtualenv -p /usr/bin/python3.6 venv && source virtual/bin/activate
+```
+
+### Install dependancies
+Install dependancies that will create an environment for the app to run
+`pip3 install -r requirements.txt`
+
+### Create the Database
+```bash
+psql
+CREATE DATABASE hood;
+```
+
+```
+### Run initial Migration
+```bash
+python3.6 manage.py makemigrations hoodwatch
+python3.6 manage.py migrate
+```
+
+### Run the app
+```bash
+python3.6 manage.py runserver
+```
+Open terminal on `localhost:8000`
+
+## Known bugs
+No known bugs so far
+
+## Technologies used
+    - HTML
+    - JavaScript
+    - css
+    - Python 3.6
+    - Bootstrap 4
+    - Heroku
+    - Postgresql
+
+
+### License
+Copyright (c) **Jackson kitsao**
--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -1,39 +1,136 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from hoodwatch.models import Post,Profile,Neighbourhood,Business,Join
+from django.http import HttpResponse
+from .models import Post,Profile,Neighbourhood,Business,Join
+from django.contrib import messages
+from . forms import NewPostForm,UserForm,CreateHoodForm,ProfileForm
+from django.contrib.auth.models import User
 
 
-def register(request):
+def index(request):
+    hoods = Neighbourhood.objects.all()
+    business = Business.objects.all()
+    posts = Post.objects.all()
+    print(posts)
+    return render(request, 'index.html',locals())
+
+@login_required(login_url='/accounts/login/')
+def profile(request,user_id=None):
+    if user_id == None:
+        user_id=request.user.id
+    current_user = User.objects.get(id = user_id)
+    user = current_user
+    images = Post.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login')
+def updateprofile(request):
+	if request.method == 'POST':
+		form = ProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('profile')
+
+	else:
+			form = ProfileForm()
+	return render(request, 'updateprofile.html',{"form":form })
+
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = NewPostForm(request.POST, request.FILES)
+
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your Account has been created! You are able to log in {username}!')
-            return redirect('login') 
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            return redirect('hoods')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form":form})
 
-@login_required
-def profile(request):
+@login_required(login_url='/accounts/login/')
+def createHood(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your Account has been Updated!')
-            return redirect('profile') 
+        form = CreateHoodForm(request.POST)
+        if form.is_valid():
+            hood = form.save(commit = False)
+            hood.user = request.user
+            hood.save()
+       
+        return redirect('index')
+    else:
+        form = CreateHoodForm()
+        return render(request,'new_hood.html',{"form":form})
+
+def search(request):
+
+    if request.GET['hoods']:
+        search_term = request.GET.get("hoods")
+        hoods = Neighbourhood.search_hood(search_term)
+        message = f"{search_term}"
+
+        return render(request,'search.html',locals())
 
     else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-    
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
+        message = "You Haven't searched for any item"
+        return render(request,'search.html',locals())
 
-    return render(request, 'users/profile.html', context)
+
+  
+@login_required(login_url = '/accounts/login')
+def all_hoods(request):
+
+    if request.user.is_authenticated:
+        if Join.objects.filter(user_id=request.user).exists():
+            hood = Neighbourhood.objects.get(pk=request.user.join.hood_id.id)
+            businesses = Business.objects.filter(hood=request.user.join.hood_id.id)
+            posts = Post.objects.filter(hood=request.user.join.hood_id.id)
+            print(posts)
+            return render(request, "hood.html", locals())
+        else:
+            neighbourhoods = Neighbourhood.objects.all()
+            return render(request, 'hood.html', locals())
+    else:
+        neighbourhoods = Neighbourhood.objects.all()
+
+        return render(request, 'hood.html', locals())
+
+
+
+def create_business(request):
+    current_user = request.user
+    print(Profile.objects.all())
+    owner = Profile.get_by_id(current_user)
+    # this_hood = Neighbourhood.objects.all()
+    if request.method == 'POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            new_biz=form.save(commit=False)
+            new_biz.user = current_user
+            # new_biz.hood =this_hood
+            new_biz.save()
+            return redirect(home)
+    else:
+        form = BusinessForm()
+    return render(request,"businessform.html",locals())
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def join(request, hoodId):
+    neighbourhood = Neighbourhood.objects.get(pk=hoodId)
+    if Join.objects.filter(user_id=request.user).exists():
+
+        Join.objects.filter(user_id=request.user).update(hood_id=neighbourhood)
+    else:
+
+        Join(user_id=request.user, hood_id=neighbourhood).save()
+
+    messages.success(request, 'Success! You have succesfully joined this Neighbourhood ')
+    return redirect('hoods')
